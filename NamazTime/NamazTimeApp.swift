ef31183b4d1af7b0ec82e82105d1c3528e32052c
Rehnuma Reza (Deepty) <rehnuma.deepty@therapservices.net>
2024-10-01 10:32:14 +0600

    Initial Commit

---
+++ NamazTime/NamazTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  NamazTimeApp.swift
+//  NamazTime
+//
+//  Created by Rehnuma Reza Deepty on 1/10/24.
+//
+
+import SwiftUI
+
+@main
+struct NamazTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
